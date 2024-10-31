6bcaa47ce0fce6b244589bf1e5dc90d253a27a18
Haider Mumtaz <haidermumtaz98@gmail.com>
2024-10-31 19:25:17 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,3 +2,5 @@ import numpy as np
 import pandas as pd
 
 
+playerStats = pd.read_csv('C:/Users/haide/Documents/GitHub/Python-Data-Analysis-Project/vctDataSet/Datasets/vct_2024/players_stats/players_stats.csv')
+
