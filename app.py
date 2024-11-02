7046b0977affc89c76d23b23059dd1e450cc09d6
Haider Mumtaz <haidermumtaz98@gmail.com>
2024-11-01 22:47:32 -0400

    data visualizations
    trying to visualize the data in a nicer way than just a table lol

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from matplotlib import pyplot as plt
+import seaborn as sns
+
 
 
 player_stats_df = pd.read_csv('C:/Users/haide/Documents/GitHub/Python-Data-Analysis-Project/vctDataSet/Datasets/vct_2024/players_stats/players_stats.csv')
@@ -25,3 +27,27 @@ first_engagements['First Engagement Ratio'] = first_engagements['First Kills'] /
 top_first_engagers = first_engagements.sort_values(by=['First Engagement Ratio'], ascending=False).drop_duplicates(subset=['Player']).head(10)
 
 print(top_first_engagers)
+
+
+
+# Data for the Top-Rated Players: Visualizing Rating and Kills:Deaths
+fig, axes = plt.subplots(1, 2, figsize=(14, 6))
+fig.suptitle("Top Players by Rating and First Engagement Performance")
+
+# Plot 1: Top 10 Players by Rating with their K/D Ratios
+sns.barplot(
+    data=top_rated_players.sort_values("Rating", ascending=False),
+    x="Rating",
+    y="Player",
+    hue="Kills:Deaths",
+    palette="viridis",
+    ax=axes[0]
+)
+axes[0].set_title("Top-Rated Players with K/D Ratios")
+axes[0].set_xlabel("Player Rating")
+axes[0].set_ylabel("Player")
+axes[0].legend(title="Kills:Deaths Ratio")
+
+
+
+plt.show()
